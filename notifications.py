5e2a95089ce1f1a9e9f0e401a5419a16762b566c
Yakshit Poojary <yakshitpoojary2004@gmail.com>
2022-06-23 20:25:33 +0530

    None

--- notifications.py
+++ notifications.py
@@ -3,7 +3,6 @@ from sys import exit
 
 def popupError(s):
     popupRoot = Tk()
-    popupRoot.after(2000, exit)
     popupButton = Button(popupRoot, text = s, font = ("Verdana", 16, 'bold'), bg = "sky blue", command = exit)
     popupButton.pack()
     popupRoot.geometry('400x50+700+500')
