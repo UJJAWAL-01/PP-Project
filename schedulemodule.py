fab24f2baf51d94ae5b66ce0ba20ac315f250988
Yakshit Poojary <yakshitpoojary2004@gmail.com>
2022-06-23 22:44:51 +0530

    Minor fixes

--- schedulemodule.py
+++ schedulemodule.py
@@ -2,23 +2,31 @@ import time
 import schedule
 from notifypy import Notify
 
+#Made Lists and Dictionary to store date,time,tasks and number of tasks
 timetable = {}
 keys = []
 task = []
+days=[]
 no_of_tasks=[]
 
-n=int(input("Enter number of days between 1 to 7: "))
-days=[]
+#To input number of days,day and number of tasks for that day.
+j=0
+while(j==0):
+    n=int(input("Enter number of days between 1 to 7: "))
 
-if(n>0 and n<=7):
+    if(n<0 or n>7):
+        print("Invalid")
+        continue 
+
     for i in range(n):
         day=input("Enter day: ")
+        day=day.lower()
         days.append(day)
         num = int(input("enter the number of tasks on " + days[i]+": "))
         no_of_tasks.append(num)
-else:
-        print("Please input number of days between 1-7!!")
+        j=1
 
+#To input the time and task
 for i in range(len(days)):
     for j in range(len(no_of_tasks)-1):
         t = input("Enter the time on "+days[i]+": ")
@@ -26,6 +34,7 @@ for i in range(len(days)):
         w = input("Enter the task: ")
         task.append(w)
 
+#Storing time and task values in dictionary
 for i in range(len(keys)):
     timetable[keys[i]] = task[i]
 
@@ -33,12 +42,14 @@ for i in range(len(keys)):
 for i in range(len(keys)):
     print("You have "+ task[i] +" in " + keys[i])
 
+#Function for notification
 def notification(task):
     notification = Notify()
     notification.title = "Productivity Manager"
     notification.message = "It is time for your "+task
     notification.send()
 
+#Scheduling notifications
 i=0
 while i<len(keys):
     schedule.every().day.at(keys[i]).do(notification,task[i])
